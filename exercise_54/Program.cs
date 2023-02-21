16300e4b8702d19f74c7516da02558be7ed0ae60
Nikita Karetkin <nikita-kareta@yandex.ru>
2023-02-21 23:39:10 +0300

    exercise 54 had been accomplished

---
+++ exercise_54/Program.cs
@@ -0,0 +1,53 @@
+﻿int[,] RandomFillArray()
+{
+    int[,] Array = new int[4, 4];
+
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            Array[i, j] = new Random().Next(10);
+            Console.Write($"{Array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    return Array;
+}
+
+int[,] LessOrder(int[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            for (int k = 0; k < Array.GetLength(1) - 1; k++)
+            {
+                if (Array[i, k] < Array[i, k + 1])
+                {
+                    int temporary = Array[i, k + 1];
+                    Array[i, k + 1] = Array[i, k];
+                    Array[i, k] = temporary;
+                }
+            }
+        }
+    }
+    return Array;
+}
+
+
+
+void PrintArray(int[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            Console.Write($"{Array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] Array = LessOrder(RandomFillArray());
+Console.WriteLine();
+PrintArray(Array);
