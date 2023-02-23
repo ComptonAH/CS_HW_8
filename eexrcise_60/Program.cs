4a823c4048eff7d505ee43fb08d9b1092b301769
Nikita Karetkin <nikita-kareta@yandex.ru>
2023-02-23 17:20:17 +0300

    ...

---
+++ eexrcise_60/Program.cs
@@ -0,0 +1,23 @@
+﻿void PrintMultiArray(int[] Array)
+{
+    int[,,] MultiArray = new int[2, 2, 2];
+
+    int[] temp = new int[8];
+    for (int i = 0; i < MultiArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < MultiArray.GetLength(1); j++)
+        {
+            for (int k = 0; k < MultiArray.GetLength(2); k++)
+            {
+                MultiArray[i, j, k] = Convert.ToInt32(Array.GetValue(new Random().Next(1, 90)));
+                Console.Write($"{MultiArray[i, j, k]}({i},{j},{k}) ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+
+var random = new Random().Next(10, 100);
+var intArray = Enumerable.Range(10, 90).OrderBy(t => random).ToArray();
+PrintMultiArray(intArray);
---
+++ exercise_62/Program.cs
@@ -0,0 +1,56 @@
+﻿int[,] Array = new int[4, 4];
+int addition = 1;
+
+for (int i = 0, j = 0; j < Array.GetLength(1); j++)
+{
+    Array[i, j] = addition;
+    addition++;
+}
+
+for (int i = 1, j = Array.GetLength(1) - 1; i < Array.GetLength(0); i++)
+{
+    Array[i, j] = addition;
+    addition++;
+}
+
+for (int i = Array.GetLength(0) - 1, j = Array.GetLength(1) - 2; j >= 0; j--)
+{
+    Array[i, j] = addition;
+    addition++;
+}
+
+for (int i = Array.GetLength(0) - 2, j = 0; i > 0; i--)
+{
+    Array[i, j] = addition;
+    addition++;
+}
+
+for (int i = 1, j = 1; j <= Array.GetLength(1) - 2; j++)
+{
+    Array[i, j] = addition;
+    addition++;
+}
+
+for (int i = Array.GetLength(0) - 2, j = Array.GetLength(1) - 2; i <= Array.GetLength(1) - 2; i++)
+{
+    Array[i, j] = addition;
+    addition++;
+}
+
+for (int i = Array.GetLength(0) - 2, j = Array.GetLength(1) - 3; j != Array.GetLength(1) - 2; j++)
+{
+    Array[i, j] = addition;
+}
+
+for (int i = 0; i < Array.GetLength(0); i++)
+{
+    for (int j = 0; j < Array.GetLength(1); j++)
+    {
+        if (Array[i, j] < 10)
+        {
+            Console.Write($"{0}{Array[i, j]} ");
+        }
+        else Console.Write($"{Array[i, j]} ");
+    }
+    Console.WriteLine();
+}
